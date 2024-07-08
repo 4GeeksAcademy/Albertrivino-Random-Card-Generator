73ee955169582bdd8cf83923a09a93efb7e73768
Albertrivino <155556342+Albertrivino@users.noreply.github.com>
2024-07-08 08:35:25 +0000

    [Albertrivino] -Project Random Card Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let cardIcon = document.querySelectorAll(".icon");
+  let cardNumber = document.querySelector(".number");
+
+  const icon = ["♦", "♥", "♠", "♣"];
+  const numbers = ["A", "J", "Q", "K", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  function getRandomItem(list) {
+    return list[Math.floor(Math.random() * list.length)];
+  }
+
+  function generateCard() {
+    let randomIcon = getRandomItem(icon);
+    let randomNumber = getRandomItem(numbers);
+    let color = randomIcon === "♦" || randomIcon === "♥" ? "red" : "black";
+
+    cardNumber.innerHTML = randomNumber;
+    cardNumber.style.color = color;
+
+    cardIcon.forEach(iconElement => {
+      iconElement.innerHTML = randomIcon;
+      iconElement.style.color = color;
+    });
+  }
+  generateCard();
 };
